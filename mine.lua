e63ebb2d747b592ff4df5eb0c9e40e72d917d7b9
eriksters <eriksters@gmail.com>
2024-12-27 01:39:53 +0100

    com

--- mine.lua
+++ mine.lua
@@ -1,17 +1,16 @@
+local MAX_MOVE_RETRIES = 10
+
 local args = {...}
 local length = args[1]
 local width = args[2]
-local count = args[3]
-local height = args[4]
+local height = args[3]
 
 if length == nil then length = 2 end
-if width == nil then width = 0 end 
-if count == nil then count = 0 end
-if height == nil then height = 1 end
+if width == nil then width = 0 end
+if height == nil then height = 2 end
 
 length = math.floor(length)
 width = math.floor(width)
-count = math.floor(count)
 height = math.floor(height)
 
 if length < 2 then
@@ -22,10 +21,138 @@ if width < 0 then
     error("Width must be at least 0")
 end
 
-if count < 0 then
-    error("Count must be at least 0")
-end
-
-if height < 1 then
+if height < 12 then
     error("Height must be at least 1")
 end
+
+function moveUp()
+    for i = 1, MAX_MOVE_RETRIES do
+        local success, result = turtle.up()
+        if success then
+            return
+        else
+            turtle.digUp()
+        end
+    end
+    print("Failed to move up")
+end
+
+function moveDown()
+    for i = 1, MAX_MOVE_RETRIES do
+        local success, result = turtle.down()
+        if success then
+            return
+        else
+            turtle.digDown()
+        end
+    end
+    print("Failed to move down")
+end
+
+function moveForward()
+    for i = 1, MAX_MOVE_RETRIES do
+        local success, result = turtle.forward()
+        if success then
+            return
+        else
+            turtle.dig()
+        end
+    end
+    print("Failed to move forward")
+end
+
+function digColumn()
+    turtle.digDown()
+    local moveHeight = height - 2
+    for i = 0, moveHeight do
+        turtle.digUp()
+        moveUp()
+    end
+    if height > 2 then
+        turtle.digUp()
+    end
+    for i = 0, moveHeight do
+        moveDown()
+    end
+end 
+
+--[[
+    STARTUP
+]]
+turtle.digUp()
+moveUp()
+
+--[[
+    MAIN LOOP
+]]
+local row_count = 0
+while true do
+    local did_tunnel_connect = false
+    
+    if row_count ~= 0 then
+        turtle.turnLeft()
+    end
+
+    for i = 0, length do
+        digColumn()
+        moveForward()
+    end
+    digColumn()
+
+    if row_count ~= 0 and width > 0 then
+        turtle.turnLeft()
+        for i = 0, width do
+            turtle.dig()
+            moveForward()
+            digColumn()
+        end
+        turtle.turnRight()
+        turtle.turnRight()
+        for i = 0, width do
+            moveForward()
+        end
+        did_tunnel_connect = true
+    end
+
+    if not did_tunnel_connect then
+        turtle.turnRight()
+    end
+
+    for i = 0, width + 1 do
+        turtle.dig()
+        moveForward()
+        digColumn()
+    end
+
+    turtle.turnRight()
+
+    for i = 0, length do
+        if i ~= 0 then
+            digColumn()
+        end
+        moveForward()
+    end
+
+    turtle.turnRight()
+
+    for i = 0, width do
+        turtle.dig()
+        moveForward()
+        digColumn()
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i = 0, width do
+        moveForward()
+    end
+    for i = 0, width + 1 do
+        turtle.dig()
+        moveForward()
+        digColumn()
+    end
+
+    turtle.turnLeft()
+
+    row_count = row_count + 1
+end
+
