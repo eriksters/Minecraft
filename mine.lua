f6e1712fd5d697b37b2aece27602f976d6e50b4e
eriksters <eriksters@gmail.com>
2024-12-28 01:31:13 +0100

    com

--- mine.lua
+++ mine.lua
@@ -5,6 +5,10 @@ local length = args[1]
 local width = args[2]
 local height = args[3]
 
+local direction = 0 -- 0: x+, 1: z+, 2: x-, 3: z-
+local x = 0
+local z = 0
+local y = 0
 if length == nil then length = 2 end
 if width == nil then width = 0 end
 if height == nil then height = 2 end
@@ -29,9 +33,11 @@ function moveUp()
     for i = 1, MAX_MOVE_RETRIES do
         local success, result = turtle.up()
         if success then
+            y = y + 1
             return
         else
             turtle.digUp()
+            turtle.attackUp()
         end
     end
     print("Failed to move up")
@@ -41,9 +47,11 @@ function moveDown()
     for i = 1, MAX_MOVE_RETRIES do
         local success, result = turtle.down()
         if success then
+            y = y - 1
             return
         else
             turtle.digDown()
+            turtle.attackDown()
         end
     end
     print("Failed to move down")
@@ -53,12 +61,32 @@ function moveForward()
     for i = 1, MAX_MOVE_RETRIES do
         local success, result = turtle.forward()
         if success then
+            if direction == 0 then
+                x = x + 1
+            elseif direction == 1 then
+                z = z + 1
+            elseif direction == 2 then
+                x = x - 1
+            elseif direction == 3 then
+                z = z - 1
+            end
             return
         else
             turtle.dig()
+            turtle.attack()
         end
     end
     print("Failed to move forward")
+end
+
+function turnLeft()
+    turtle.turnLeft()
+    direction = (direction + 3) % 4
+end
+
+function turnRight()
+    turtle.turnRight()
+    direction = (direction + 1) % 4
 end
 
 function digColumn()
@@ -74,7 +102,43 @@ function digColumn()
     for i = 1, moveHeight do
         moveDown()
     end
-end 
+end
+
+function tunnel(l, h)
+    local moveHeight = h - 3
+    local block_count = 0
+    local pos = "up"                -- up, down
+
+    while block_count < l do
+        if block_count ~= 0 then
+            turtle.dig()
+            moveForward()
+        end
+        if pos == "up" then
+            if l > 2 then
+                turtle.digUp()
+            end
+            for i = 1, moveHeight do
+                turtle.digDown()
+                moveDown()
+            end
+            pos = "down"
+        elseif pos == "down" then
+            turtle.digDown()
+            for i = 1, moveHeight do
+                turtle.digUp()
+                moveUp()
+            end
+            pos = "up"
+        end
+        block_count = block_count + 1
+    end
+    if pos == "up" then
+        for i = 1, moveHeight do
+            moveDown()
+        end
+    end
+end
 
 --[[
     STARTUP
@@ -89,21 +153,13 @@ local row_count = 0
 while true do
     local did_tunnel_connect = false
 
-    for i = 1, length do
-        digColumn()
-        moveForward()
-    end
-    digColumn()
+    tunnel(length, height)
 
     if row_count ~= 0 and width > 0 then
-        turtle.turnLeft()
-        for i = 1, width do
-            turtle.dig()
-            moveForward()
-            digColumn()
-        end
-        turtle.turnRight()
-        turtle.turnRight()
+        turnLeft()
+        tunnel(width, height)
+        turnRight()
+        turnRight()
         for i = 1, width do
             moveForward()
         end
@@ -111,7 +167,7 @@ while true do
     end
 
     if not did_tunnel_connect then
-        turtle.turnRight()
+        turnRight()
     end
 
     for i = 1, width + 1 do
@@ -120,7 +176,7 @@ while true do
         digColumn()
     end
 
-    turtle.turnRight()
+    turnRight()
 
     for i = 1, length do
         if i ~= 0 then
@@ -130,15 +186,15 @@ while true do
     end
     digColumn()
 
-    turtle.turnRight()
+    turnRight()
 
     for i = 1, width do
         turtle.dig()
         moveForward()
         digColumn()
     end
-    turtle.turnRight()
-    turtle.turnRight()
+    turnRight()
+    turnRight()
     for i = 1, width do
         moveForward()
     end
@@ -148,7 +204,7 @@ while true do
         digColumn()
     end
 
-    turtle.turnLeft()
+    turnLeft()
 
     row_count = row_count + 1
 end
